7f770f7a4463f7ed85f752fd8deb7b314aadf7d2
Vedangmodi <modivedang.mv94@gmail.com>
2025-10-27 15:35:32 +0530

    Time: 12 ms (53.73%) | Memory: 45 MB (89.08%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,40 +1,64 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int[] arr = new int[bank.length];
-        int x = 0;
+        // int[] arr = new int[bank.length];
+        // int x = 0;
+
+        // for(String str : bank){
+        //     for(int i=0; i<str.length(); i++){
+        //         if(str.charAt(i) == '1'){
+        //             arr[x]++;
+        //         }
+
+        //     }
+        //     x++;
+        // }
+
+        // int count = 0;
+        // int i=0; 
+        // int j=1;
+
+        // while(j < arr.length){
+        //     if(arr[j] == 0){
+        //         j++;
+        //     }
+        //     else if(arr[i] == 0){
+        //         i++;
+        //     }
+        //     else if(i == j){
+        //         j++;
+        //     }
+        //     else{
+        //         count = count + (arr[i] * arr[j]);
+        //         i = j;
+        //         j++;
+        //     }
+        // }
+
+        // return count;
+
+
+
+        // optimised
+
+        int prev = 0;
+        int sum = 0;
 
         for(String str : bank){
+            int curr = 0;
             for(int i=0; i<str.length(); i++){
+                
                 if(str.charAt(i) == '1'){
-                    arr[x]++;
+                    curr++;
                 }
 
             }
-            x++;
-        }
-
-        int count = 0;
-        int i=0; 
-        int j=1;
-
-        while(j < arr.length){
-            if(arr[j] == 0){
-                j++;
-            }
-            else if(arr[i] == 0){
-                i++;
-            }
-            else if(i == j){
-                j++;
-            }
-            else{
-                count = count + (arr[i] * arr[j]);
-                i = j;
-                j++;
+            if (curr > 0) {         
+                sum += prev * curr;
+                prev = curr;
             }
         }
 
-        return count;
+        return sum;
         
     }
 }
