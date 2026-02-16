10a012caf3c0551a2b9e0fa65775da38394271af
Vedang Modi <modivedang.mv94@gmail.com>
2026-02-16 22:14:34 +0530

    Time: 1 ms (60.34%) | Memory: 42.4 MB (85.32%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,11 +1,18 @@
 class Solution {
     public int reverseBits(int n) {
         int result = 0;
-        for (int i = 0; i < 32; i++) {
-            int bit = n & 1;         
-            result = (result << 1) | bit; 
-            n = n >>> 1;              
+
+        for(int i=0; i<32; i++){
+            int bits = n & 1;
+            result = (result << 1) | bits;
+
+            // >> → signed right shift (keeps sign bit)
+            // >>> → unsigned right shift (fills with 0)
+
+            n = n >>> 1;
+
         }
+
         return result;
         
     }
