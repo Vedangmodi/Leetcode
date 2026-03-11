075ca691525ecc89ef4c203e4be242683b7c19cb
Vedang Modi <modivedang.mv94@gmail.com>
2026-03-11 17:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.82%) - LeetSync

---
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int bitwiseComplement(int num) {
+        if (num == 0) return 1;
+
+        int bits = (int) Math.floor(Math.log(num)/ Math.log(2)) + 1;
+
+        int mask = (1 << bits) - 1;
+
+        return num ^ mask;
+
+
+        
+    }
+}
