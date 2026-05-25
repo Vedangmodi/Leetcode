16c2af2362053ca5742d0fca23d3bbf5cc77e3b5
Vedang Modi <modivedang.mv94@gmail.com>
2026-05-26 00:30:01 +0530

    Time: 10 ms (87.60%) | Memory: 46.4 MB (41.60%) - LeetSync

--- 1466-jump-game-v/jump-game-v.java
+++ 1466-jump-game-v/jump-game-v.java
@@ -1,43 +1,53 @@
 class Solution {
-    int n;
-    int[] t;
-
-    private int solve(int[] arr, int d, int i) {
-        if (t[i] != -1)
-            return t[i];
-
-        int result = 1; // count current index also
-
-        // move left
-        for (int j = i - 1; j >= Math.max(0, i - d); j--) {
-            // can't jump further once taller/equal element appears
-            if (arr[j] >= arr[i])
-                break;
-            result = Math.max(result, 1 + solve(arr, d, j));
-        }
-
-        // move right
-        for (int j = i + 1; j <= Math.min(n - 1, i + d); j++) {
-            // can't jump further once taller/equal element appears
-            if (arr[j] >= arr[i])
-                break;
-            result = Math.max(result, 1 + solve(arr, d, j));
-        }
-
-        return t[i] = result;
-    }
+    int[] memo;
 
     public int maxJumps(int[] arr, int d) {
-        n = arr.length;
-        t = new int[n];
-        Arrays.fill(t, -1);
+        int n = arr.length;
+        memo = new int[n];
 
-        int maxJump = 1;
-        // We need to try from every index
-        for (int i = 0; i < n; i++) {
-            maxJump = Math.max(maxJump, solve(arr, d, i));
+        Arrays.fill(memo, -1);
+
+        int res = 1;
+
+        for(int i = 0; i<n; i++){
+            res = Math.max(res,solve(arr, i, d));
         }
 
-        return maxJump;
+        return res;
+
+       
+    }
+
+    public int solve(int[] arr, int i, int d){
+
+        if(memo[i] != -1){
+            return memo[i];
+        }
+
+        int n = arr.length;
+
+        int res = 1;
+
+        // right
+        for(int j = i + 1; j <= Math.min(n - 1, i + d); j++){
+            if(arr[j] >= arr[i]){
+                break;
+            }
+
+            res = Math.max(res, 1 + solve(arr, j, d));
+        }
+
+        // left
+        for(int j = i - 1; j >= Math.max(0, i - d); j--){
+            if(arr[j] >= arr[i]){
+                break;
+            }
+
+            res = Math.max(res, 1 + solve(arr, j, d));
+        }
+
+        return memo[i] = res;
+
+
     }
 }
